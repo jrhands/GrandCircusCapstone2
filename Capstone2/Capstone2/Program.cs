766f5bf74f17d70b9b7c78796879812008a5968f
Jacob Hands <jrhands@gmail.com>
2018-07-30 08:03:25 -0400

    Wrapped up for the day

---
+++ Capstone2/Capstone2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Capstone2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Task System 3000!");
+            r
+        }
+
+        static void randomStartMessage()
+        {
+            Random random = new Random();
+            int startMessageRandomizer = random.Next(0, 3);
+            switch (startMessageRandomizer)
+            {
+                case 0:
+                    Console.WriteLine("Enterprise grade task management at your fingertips!");
+                    break;
+                case 1:
+                    Console.WriteLine("The other 2999 didn't turn out too well...");
+                    break;
+                case 2:
+                    Console.WriteLine("Don't worry, aerobics aren't on the list. Probably.");
+                    break;
+                default:
+                    Console.WriteLine("Jacob messed up the range on his start message randomizer. PM him on Slack about it and call him stinky.");
+                    break;
+            }
+        }
+    }
+}
